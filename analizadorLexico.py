62b38cc916b2302c25fdd98c04f13d834062e795
WAlejandroW <alejandrologlez11@gmail.com>
2026-08-26 00:07:39 -0600

    Se agrego una funcion muy basica para identificar caracteres especiales

--- analizadorLexico.py
+++ analizadorLexico.py
@@ -1,12 +1,22 @@
 entrada = input("Entrada: ")
 i = 0
 
+def comparar (comparar):
+    caracteres = ["+", "-", "*","/","<", ">", "=", "|", "&", "!", ";", ",", "(", ")", "{", "}", "$"]
+    x = 0
+    while x < len(caracteres):
+        if comparar == caracteres[x]:
+            return caracteres[x]
+        else:
+            x +=1
+
 while i < len(entrada):
 
+    #Identificador de espacios
     if entrada[i] == ' ':
         i += 1
         continue
-
+    #Identificador de palabras reservadas e identificadores
     if entrada[i].isalpha():
         token = ""
 
@@ -15,14 +25,14 @@ while i < len(entrada):
             i += 1
 
         print(token, "-> IDENTIFICADOR")
-
+    #Identificador de enteros
     elif entrada[i].isdigit():
         token = ""
 
         while i < len(entrada) and entrada[i].isdigit():
             token += entrada[i]
             i += 1
-
+        #Identificador de Reales
         if i < len(entrada) and entrada[i] == '.':
             token += '.'
             i += 1 
@@ -35,7 +45,21 @@ while i < len(entrada):
 
         else:
             print(token, "-> ENTERO")
-
+    #Identificador de caracteres especiales 
     else:
-        print(entrada[i], "-> ERROR")
-        i += 1
+        token = comparar(entrada[i])
+        i +=1 
+        contador = 0
+        while contador < 2:
+            if token == comparar(entrada[i]):
+                token += entrada[i]
+                i += 1
+                contador += 1
+            else:
+                token += comparar(entrada[i])
+                i += 1
+                contador += 1
+                break
+        print(token, " -> Caracter especial")
+
+    
